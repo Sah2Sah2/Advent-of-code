0c36bf07fbb3a27b33ef099cfe137ab22353541e
Sah2Sah2 <sara.battistella@iths.se>
2025-12-11 16:18:19 +0100

    day 2 part 2

---
+++ 2025/Day2/Part2.ts
@@ -0,0 +1,49 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+
+const inputPath = join(__dirname, "DayOneInput.txt");
+const content: string = readFileSync(inputPath, "utf-8");
+const lines: string[] = content.split(/\r?\n/);
+
+const line = lines[0];
+
+const rangeStrings = line.split(",");
+
+const cleanedRanges = rangeStrings.filter((r) => r.trim() !== "");
+
+const ranges = cleanedRanges.map((r) => {
+  const [startStr, endStr] = r.split("-");
+  return {
+    start: Number(startStr),
+    end: Number(endStr),
+  };
+});
+
+function isInvalid(id: number): boolean {
+  const s = id.toString();
+  const len = s.length;
+
+  for (let groupSize = 1; groupSize <= Math.floor(len / 2); groupSize++) {
+    if (len % groupSize !== 0) continue;
+
+    const group = s.slice(0, groupSize);
+    const repeats = len / groupSize;
+
+    if (group.repeat(repeats) === s) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+let total = 0;
+for (const range of ranges) {
+  for (let id = range.start; id <= range.end; id++) {
+    if (isInvalid(id)) {
+      total += id;
+    }
+  }
+}
+
+console.log(total);
